d32a3f47d200956da6009ca06519e8d2daba0994
vaibhavvemani <59450124+vaibhavvemani@users.noreply.github.com>
2023-04-01 20:12:56 +0530

    2.

--- main.js
+++ main.js
@@ -6,3 +6,49 @@ function getUserName() {
 }
 submitBtnEl.addEventListener('click', getUserName);
 
+let playerScore = 0, computerScore = 0;
+const choices = ['Rock', 'Paper', 'Sicssor'];
+
+const rockBtn = document.getElementById('rock');
+const paperBtn = document.getElementById('paper');
+const sicssorBtn = document.getElementById('sicssor');
+
+function checkWinner(usr, comp){
+    if((usr === 1 && comp === 3) || (usr === 2 && comp === 1) || (usr === 3 && comp === 2)){
+        playerScore += 1;
+        document.getElementById('playerScore').innerHTML = `Player score is ${playerScore}`;
+    } else if((usr === 1 && comp === 2) || (usr === 2 && comp === 3) || (usr === 3 && comp === 1)){
+        computerScore += 1;
+        document.getElementById('compScore').innerHTML = `Computer score is ${computerScore}`;
+    }
+}
+
+function computerSelect(usr) {
+
+    checkWinner(usr, comp);
+
+    let randNum = Math.floor(Math.random(1) * 3);
+    let comp = randNum + 1;
+    let computerChoice = choices[randNum];
+    document.getElementById('computerChoice').innerHTML = `Computer chose ${computerChoice}`;
+}
+
+function choiceRock() {
+    document.getElementById('userChoice').innerHTML = `You chose rock`;
+    computerSelect(1);
+}
+
+function choicePaper() {
+    document.getElementById('userChoice').innerHTML = `You chose paper`;
+    computerSelect(2);
+}
+
+function choiceSicssor() {
+    document.getElementById('userChoice').innerHTML = `You chose sicssor`;
+    computerSelect(3);
+}
+
+
+rockBtn.addEventListener('click', choiceRock);
+paperBtn.addEventListener('click', choicePaper);
+sicssorBtn.addEventListener('click', choiceSicssor);
