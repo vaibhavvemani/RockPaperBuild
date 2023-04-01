c2aba4a08acedbdc562281cc81208b3f8e6a70ff
vaibhavvemani <59450124+vaibhavvemani@users.noreply.github.com>
2023-04-01 21:45:23 +0530

    3.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const inputText = document.getElementById('uname');
 const submitBtnEl = document.getElementById('getInfo');
 const outputEl = document.getElementById('displayMessage');
+
 function getUserName() {
     outputEl.innerHTML = `Hi ${inputText.value}! Choose an option`;
 }
@@ -16,21 +17,25 @@ const sicssorBtn = document.getElementById('sicssor');
 function checkWinner(usr, comp){
     if((usr === 1 && comp === 3) || (usr === 2 && comp === 1) || (usr === 3 && comp === 2)){
         playerScore += 1;
+        document.getElementById('winner').innerHTML = `${inputText.value} won`;
         document.getElementById('playerScore').innerHTML = `Player score is ${playerScore}`;
     } else if((usr === 1 && comp === 2) || (usr === 2 && comp === 3) || (usr === 3 && comp === 1)){
         computerScore += 1;
         document.getElementById('compScore').innerHTML = `Computer score is ${computerScore}`;
+        document.getElementById('winner').innerHTML = `Computer won`;
+    } else{
+        document.getElementById('winner').innerHTML = `It\'s a tie`;
     }
 }
 
 function computerSelect(usr) {
 
-    checkWinner(usr, comp);
-
     let randNum = Math.floor(Math.random(1) * 3);
     let comp = randNum + 1;
     let computerChoice = choices[randNum];
     document.getElementById('computerChoice').innerHTML = `Computer chose ${computerChoice}`;
+
+    checkWinner(usr, comp);
 }
 
 function choiceRock() {
